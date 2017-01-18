fa72eb19a90668143a6f3e6d276ac0a41013e060
igabchev87 <iganchev87@gmail.com>
2017-01-18 22:39:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 0, factorial = 0, counter = 0;
+            Console.Write("n= ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            factorial = num;
+            counter = num - 1;
+            while (counter > 0)
+            {
+                factorial *= counter;
+                counter--;
+
+            }
+            Console.WriteLine("{0}!={1}",num,factorial);
+        }
+    }
+}
